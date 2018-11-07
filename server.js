9c09472ea71a082b71d4e712f60461f919d4f76a
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2018-11-07 14:53:53 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  res.render('maintenance', {
+    titlePage: 'Maintenance',
+    welcomeMessage: 'The site is under maintenance'
+  });
+  // next();
+});
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'Welcome to our new website',
+    titlePage: 'Home'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'About Page',
+    titlePage: 'About'
+  });
+});
+
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle the request'
+  });
+});
+
+app.listen(3000);
